3d0e45679027a2c394ff5793c1131aefc81d2e25
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-13 23:55:47 -0400

    refactor(App): add initial products

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,138 @@ import { searchProducts } from "./search-products"
 import type { Product } from "./types"
 import "./App.css"
 
+const INITIAL_PRODUCTS = [
+  {
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/1/1.jpg",
+      "https://i.dummyjson.com/data/products/1/2.jpg",
+      "https://i.dummyjson.com/data/products/1/3.jpg",
+      "https://i.dummyjson.com/data/products/1/4.jpg",
+      "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    ],
+  },
+  {
+    id: 2,
+    title: "iPhone X",
+    description:
+      "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+    price: 899,
+    discountPercentage: 17.94,
+    rating: 4.44,
+    stock: 34,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/2/1.jpg",
+      "https://i.dummyjson.com/data/products/2/2.jpg",
+      "https://i.dummyjson.com/data/products/2/3.jpg",
+      "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+    ],
+  },
+  {
+    id: 3,
+    title: "Samsung Universe 9",
+    description:
+      "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    discountPercentage: 15.46,
+    rating: 4.09,
+    stock: 36,
+    brand: "Samsung",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+    images: ["https://i.dummyjson.com/data/products/3/1.jpg"],
+  },
+  {
+    id: 4,
+    title: "OPPOF19",
+    description: "OPPO F19 is officially announced on April 2021.",
+    price: 280,
+    discountPercentage: 17.91,
+    rating: 4.3,
+    stock: 123,
+    brand: "OPPO",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/4/1.jpg",
+      "https://i.dummyjson.com/data/products/4/2.jpg",
+      "https://i.dummyjson.com/data/products/4/3.jpg",
+      "https://i.dummyjson.com/data/products/4/4.jpg",
+      "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+    ],
+  },
+  {
+    id: 5,
+    title: "Huawei P30",
+    description:
+      "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    price: 499,
+    discountPercentage: 10.58,
+    rating: 4.09,
+    stock: 32,
+    brand: "Huawei",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/5/1.jpg",
+      "https://i.dummyjson.com/data/products/5/2.jpg",
+      "https://i.dummyjson.com/data/products/5/3.jpg",
+    ],
+  },
+  {
+    id: 6,
+    title: "MacBook Pro",
+    description:
+      "MacBook Pro 2021 with mini-LED display may launch between September, November",
+    price: 1749,
+    discountPercentage: 11.02,
+    rating: 4.57,
+    stock: 83,
+    brand: "Apple",
+    category: "laptops",
+    thumbnail: "https://i.dummyjson.com/data/products/6/thumbnail.png",
+    images: [
+      "https://i.dummyjson.com/data/products/6/1.png",
+      "https://i.dummyjson.com/data/products/6/2.jpg",
+      "https://i.dummyjson.com/data/products/6/3.png",
+      "https://i.dummyjson.com/data/products/6/4.jpg",
+    ],
+  },
+  {
+    id: 7,
+    title: "Samsung Galaxy Book",
+    description:
+      "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+    price: 1499,
+    discountPercentage: 4.15,
+    rating: 4.25,
+    stock: 50,
+    brand: "Samsung",
+    category: "laptops",
+    thumbnail: "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/7/1.jpg",
+      "https://i.dummyjson.com/data/products/7/2.jpg",
+      "https://i.dummyjson.com/data/products/7/3.jpg",
+      "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+    ],
+  },
+]
+
 const App = () => {
-  const [products, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<Product[]>(INITIAL_PRODUCTS)
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
@@ -47,9 +177,9 @@ const App = () => {
         <span>Console</span> and <span>Network</span> tabs!
       </p>
       <p>
-        You only need to provide an unique <code>signalKey</code> and{" "}
-        <code>strong-fetch</code> will do the rest :). No more creating{" "}
-        <code>AbortController</code>s everywhere in your code.
+        You only need to provide an unique <code>signalKey</code> for each
+        request and <code>strong-fetch</code> will do the rest :). No more
+        creating <code>AbortController</code> instances everywhere in your code.
       </p>
       <p>
         It provides automatic JSON data handling in response and also, a{" "}
@@ -60,6 +190,13 @@ const App = () => {
       <p>
         Check the <code>strong-fetch</code> folder to check out the code!
       </p>
+      <a
+        href="https://github.com/ivanatias/strong-fetch"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Github repository
+      </a>
       <form role="search" onSubmit={(e) => e.preventDefault()}>
         <input
           placeholder="Try looking for products!"
@@ -67,7 +204,7 @@ const App = () => {
           onChange={handleSearch}
         />
       </form>
-      {products.length > 0 && <pre>{JSON.stringify(products, null, 2)}</pre>}
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </section>
   )
 }
